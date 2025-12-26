dfab3a0895b099d628e124fcc4abfe8c053bdd49
Alex Metelli <alex-metelli@gmx.com>
2025-12-26 13:17:30 +0800

    Move analytics components to widgets/analytics
    - Move FeeByMarket, FeeCollectionLog, FeeRevenueChart, ImpliedRateChart to widgets/analytics/
    - Move IndexerStatusBanner, ProtocolStats, ProtocolTvlCard to widgets/analytics/
    - Move RateHistoryTable, RateSparkline, SwapHistoryTable, TransactionHistory to widgets/analytics/
    - Move TvlBreakdown, TvlChart, VolumeByMarket, VolumeChart, VolumeStatsCard to widgets/analytics/
    - Update barrel exports
    - Remove old components/analytics/ files

--- packages/frontend/src/widgets/analytics/index.ts
+++ packages/frontend/src/widgets/analytics/index.ts
@@ -1,1 +1,17 @@
-// Widget public API - exports will be added as files are migrated
+// Analytics widgets - page sections for analytics views
+export { FeeByMarket, FeeByMarketCompact } from './FeeByMarket';
+export { FeeCollectionLog, FeeCollectionInline } from './FeeCollectionLog';
+export { FeeRevenueChart, FeeStatsCard } from './FeeRevenueChart';
+export { ImpliedRateChart, ImpliedRateCompact } from './ImpliedRateChart';
+export { IndexerStatusBanner } from './IndexerStatusBanner';
+export { ProtocolStats, ProtocolStatsCompact } from './ProtocolStats';
+export { ProtocolTvlCard, ProtocolTvlInline } from './ProtocolTvlCard';
+export { RateHistoryTable, RateHistoryInline } from './RateHistoryTable';
+export { RateSparkline, RateSparklineLarge, RateBadgeWithSparkline } from './RateSparkline';
+export { SwapHistoryTable } from './SwapHistoryTable';
+export { TransactionHistory } from './TransactionHistory';
+export { TvlBreakdown, TvlBreakdownCompact } from './TvlBreakdown';
+export { TvlChart, TvlSparkline } from './TvlChart';
+export { VolumeByMarket, VolumeBreakdownCompact } from './VolumeByMarket';
+export { VolumeChart, VolumeStackedChart } from './VolumeChart';
+export { VolumeStatsCard, VolumeInline } from './VolumeStatsCard';
